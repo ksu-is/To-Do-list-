9a19b84f71604fbd1627e70726ecd344707a610e
aosojkic <142635607+aosojkic@users.noreply.github.com>
2023-11-20 21:55:56 -0500

    maincode.py

--- maincode.py
+++ maincode.py
@@ -8,3 +8,12 @@ def main():
         show_menu()
         choice = input("Enter your choice (1-4): ")
 
+        if choice == "1":
+            print("Your to-do list is empty." if not todo_list else f"To-Do List:\n{', '.join(todo_list)}")
+        elif choice == "2":
+            todo_list.append(input("Enter the task: "))
+            print("Task added successfully!")
+        elif choice == "3":
+            print(f"Task '{todo_list.pop(int(input('Enter the task number to mark as done: ')) - 1)}' marked as done!" if todo_list else "Your to-do list is empty.")
+        elif choice == "4":
+            print("Goodbye!")
